3ab8525a3d54a10b84551021bb3f5aefaa51ff2b
Aman Kumar <aksingh230502@gmail.com>
2024-11-20 16:01:42 +0530

    Models Added

--- Models/BloodBankEntry.cs
+++ Models/BloodBankEntry.cs
@@ -2,5 +2,14 @@
 {
     public class BloodBankEntry
     {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string DonorName { get; set; }
+        public int Age { get; set; }
+        public string BloodType { get; set; }
+        public string ContactInfo { get; set; }
+        public int Quantity { get; set; }
+        public DateTime CollectionDate { get; set; }
+        public DateTime ExpirationDate { get; set; }
+        public string Status { get; set; }
     }
 }
